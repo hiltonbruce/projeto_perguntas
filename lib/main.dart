45fa16f547babd69832e1a571719f5d3bd25439f
hiltonbruce <hiltonbruce@gmail.com>
2023-01-24 20:21:58 -0300

    Botoes

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,39 @@
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(new PerguntaApp());
-}
+main() => runApp(PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
+
     return MaterialApp(
-      home: Text('Ola Flutter!!!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Column(
+          children: [
+            Text('Olá Flutter!!!'),
+            Text(perguntas[1]),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Resposta 1'),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Resposta 2'),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Resposta 3'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
