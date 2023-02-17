b9b107cdedc49ac6288ceb564f5c1ec50daacbdd
hiltonbruce <hiltonbruce@gmail.com>
2023-02-17 19:43:00 -0300

    Aula 51

--- lib/main.dart
+++ lib/main.dart
@@ -16,10 +16,22 @@ class _PerguntaAppstate extends State<PerguntaApp> {
   @override
   Widget build(BuildContext context) {
     final perguntas = [
-      'Qual é a sua cor favorita?',
-      'Qual é o seu animal favorito?',
-      'Qual é o seu endereço?',
-      'Qual é a sua casa?',
+      {
+        'texto': 'Qual é a sua cor favorita?',
+        'resposta': ['Preto', 'Vermelho', 'Verde', 'Branco']
+      },
+      {
+        'texto': 'Qual é o seu animal favorito?',
+        'resposta': ['Coellho', 'Cobra', 'Elefante', 'Leão']
+      },
+      {
+        'texto': 'Qual é o seu endereço?',
+        'resposta': ['Bayeux', 'João Pessoa', 'Campina', 'Caja']
+      },
+      {
+        'texto': 'Qual é a seu Instrutor Favorito?',
+        'resposta': ['Maria', 'João', 'Leo', 'Pedro']
+      }
     ];
     final respostas = [
       'Resposta 1',
@@ -36,7 +48,7 @@ class _PerguntaAppstate extends State<PerguntaApp> {
         body: Column(
           children: [
             Text('Olá Flutter!!!'),
-            Questao(perguntas[_perguntaSelecionada]),
+            Questao(perguntas[_perguntaSelecionada]['texto'].toString()),
             Resposta(respostas[0], _responder),
             Resposta(respostas[1], _responder),
             Resposta(respostas[2], _responder),
